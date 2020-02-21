18ec344e294c24809fd44a19d3f52f3938028473
ColdFerrin <ferrinkatz@ferrinkatz.com>
2020-02-21 14:41:11 -0700

    Add Email Send Code

--- src/Body/Contact.js
+++ src/Body/Contact.js
@@ -1,14 +1,75 @@
 import React from 'react';
+import Axios from 'axios';
 import './Contact.css';
 
 export class Contact extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+
     render () {
         return (
-            <div>
-                <p>
-                    Contact Page
-                </p>
+            <div className={"Contact"}>
+            <h2>If you need to contact me?</h2>
+                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         )
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+
+    onMessageChange(event) {
+        this.setState({message: event.target.value})
+    }
+
+    resetForm(){
+        this.setState({name:'', email:'', message:''})
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = {};
+        data.name = this.state.name;
+        data.reply_to = this.state.email;
+        data.message = this.state.message;
+
+        Axios({
+            method: "POST",
+            url:"https://bap6ezx09d.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer",
+            data:  data
+        }).then((response)=>{
+            if (response.data.status === 'success'){
+                alert("Message Sent.");
+                this.resetForm()
+            }else if(response.data.status === 'fail'){
+                alert("Message failed to send.")
+            }
+        })
     }
 }
