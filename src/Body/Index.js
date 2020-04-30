c6c06ceed8efbdaae864bde3dd317c9de5b7d51a
u_temp <e_temp>
2020-04-30 14:15:21 -0700

    add partial home page

--- src/Body/Index.js
+++ src/Body/Index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Index.css';
+import Ferrin from "./Ferrin.png"
 
 export class Index extends React.Component{
     render () {
         return (
-            <div>
-                <p>
-                    Home Page
-                </p>
+            <div className="Body-Box">
+                <div className="Body-Left">
+                    <div>
+                        <img className={"Body-Img"} src={Ferrin} alt={"ferrin headshot"}/>
+                    </div>
+                </div>
+                <div className="Body-Right">
+                    <div className="Body-Content">
+                        {/* eslint-disable-next-line no-useless-concat */}
+                        <h1 className={"inline-block"}>I'm</h1><h1 className={"color inline-block right-sp"}> Ferrin Katz</h1>
+
+                    </div>
+                </div>
             </div>
         )
     }
