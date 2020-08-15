1ce71a015694303facae1f92449aa27d46eb953b
ColdFerrin <ferrinkatz@ferrinkatz.com>
2020-08-14 19:01:23 -0700

    Add extra invisible project tile to fix spacing

--- src/Body/ProjectTile.js
+++ src/Body/ProjectTile.js
@@ -4,21 +4,31 @@ import './ProjectTile.css';
 
 export class ProjectTile extends React.Component{
     html_url;
+    // noinspection BadExpressionStatementJS
     render() {
         console.log(this.props)
-        return(
-            <div className={'outer'}>
-                <a href={this.props.data.html_url} style={{textDecoration: 'none', color: 'black'}}>
+        if(Object.keys(this.props.data).length === 0 && this.props.data.constructor === Object){
+            return(
+                <div className={'outer'}>
                     <div className={'proj'}>
-                        <h3 className={'Link-proj'}>
-                            {this.props.data.name.toString()}
-                        </h3>
-                        <p className={'Link-proj'}>
-                            {this.props.data.description.toString()}
-                        </p>
                     </div>
-                </a>
-            </div>
-        )
+                </div>
+            )
+        } else {
+            return (
+                <div className={'outer'}>
+                    <a href={this.props.data.html_url} style={{textDecoration: 'none', color: 'black'}}>
+                        <div className={'proj proj-style'}>
+                            <h3 className={'Link-proj'}>
+                                {this.props.data.name.toString()}
+                            </h3>
+                            <p className={'Link-proj'}>
+                                {this.props.data.description.toString()}
+                            </p>
+                        </div>
+                    </a>
+                </div>
+            )
+        }
     }
 }
--- src/Body/Projects.js
+++ src/Body/Projects.js
@@ -9,11 +9,17 @@ export class Projects extends React.Component{
 
         this.state = {
             data: [],
+            originalElements: 0,
+            width: 0,
+            height: 0,
             loading: true
         }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
 
     componentDidMount() {
+        this.updateWindowDimensions();
         fetch('https://api.github.com/users/ColdFerrin/repos', {
             headers: {
                 'Accept': 'application/vnd.github.v3+json'
@@ -21,9 +27,33 @@ export class Projects extends React.Component{
         })
             .then(res => res.json())
             .then((data) => {
-                this.setState({ data: data, loading: false })
+                let active = data;
+                console.log(active)
+                console.log(active.length)
+                let originalLength = active.length;
+                console.log(this.state.width)
+                let maxFit = Math.floor((this.state.width * .7) / 442);
+                console.log('maxFit = ' + maxFit)
+                let extras = ((maxFit > 0) ? (maxFit - (active.length % maxFit)) : 0);
+                console.log('extras = ' + extras)
+                console.log(typeof active)
+                for(let i = 0; i < extras; ++i){
+                    active.push({})
+                    console.log('push')
+                }
+                console.log(active.length)
+                this.setState({ data: data, originalElements: originalLength, loading: false })
             })
             .catch(console.error);
+        window.addEventListener('resize', this.updateWindowDimensions)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions)
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight })
     }
 
     render() {
